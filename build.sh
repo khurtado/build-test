5b7007be44741644ff09e46d63a5252780ba8f8a
khurtado <khurtado@nd.edu>
2017-06-13 11:10:25 -0400

    Travis mods

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@
 docker_hub="vc3project/dockerfiles"
 run_docker="docker run -u $UID:$UID -v $(pwd):$PWD -w $PWD ${docker_hub}:el6"
 $run_docker g++ hello-world.cpp -o hello.exe
+
+echo "Current files:"
+ls
