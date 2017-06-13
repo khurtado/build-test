1664cfa87836fdfc9b510bd6e43be6d7cb43a463
khurtado <khurtado@nd.edu>
2017-06-13 10:56:47 -0400

    Test docker integration

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
+# Build inside docker image
 
-g++ hello-world.cpp -o hello.exe
+docker_hub="vc3project/dockerfiles"
+run_docker="docker run -u $UID:$UID -v $(pwd):$PWD -w $PWD ${docker_hub}:el6"
+$run_docker g++ hello-world.cpp -o hello.exe
